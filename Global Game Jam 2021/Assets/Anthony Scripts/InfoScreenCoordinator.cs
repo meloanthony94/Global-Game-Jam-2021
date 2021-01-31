4b8f453d6b3e045958973cca475179389e90b582
Zeeshan Majeed <majeedzee@hotmail.com>
2021-01-31 16:05:43 -0500

    Adding more main menu polish.

---
+++ Global Game Jam 2021/Assets/Anthony Scripts/InfoScreenCoordinator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoScreenCoordinator : MonoBehaviour
+{
+    [SerializeField]
+    private Image leftInfoImage;
+    [SerializeField]
+    private Image rightInfoImage;
+
+    [SerializeField]
+    private List<Sprite> leftInfoImages = new List<Sprite>();
+    [SerializeField]
+    private List<Sprite> rightInfoImages = new List<Sprite>();
+
+    private int imageIndex = 0;
+
+    public void HandleLeftButtonPress()
+    {
+        imageIndex--;
+
+        if (imageIndex < 0)
+        {
+            imageIndex = leftInfoImages.Count - 1;
+        }
+
+        UpdateSpriteImage(imageIndex);
+    }
+
+    public void HandleRightButtonPress()
+    {
+        imageIndex++;
+
+        if (imageIndex >= leftInfoImages.Count)
+        {
+            imageIndex = 0;
+        }
+
+        UpdateSpriteImage(imageIndex);
+    }
+
+    private void UpdateSpriteImage(int imageIndex)
+    {
+        if (imageIndex >= rightInfoImages.Count)
+        {
+            rightInfoImage.enabled = false;
+        }
+        else
+        {
+            rightInfoImage.enabled = true;
+            rightInfoImage.sprite = rightInfoImages[imageIndex];
+        }
+
+        leftInfoImage.sprite = leftInfoImages[imageIndex];
+    }
+}
