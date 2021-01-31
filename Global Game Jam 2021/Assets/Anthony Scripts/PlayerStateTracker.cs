00e86a8a15408e12587e6529eb7b7b9ace205361
Zeeshan Majeed <majeedzee@hotmail.com>
2021-01-31 16:27:02 -0500

    Adding scream bar functionality.

--- Global Game Jam 2021/Assets/Anthony Scripts/PlayerStateTracker.cs
+++ Global Game Jam 2021/Assets/Anthony Scripts/PlayerStateTracker.cs
@@ -12,14 +12,24 @@ public class PlayerStateTracker : MonoBehaviour
     private Image spriteRenderer = null;
 
     [SerializeField]
+    private Image screamLevelSpriteRenderer = null;
+
+    [SerializeField]
     private Sprite playerAliveSprite = null;
     [SerializeField]
     private Sprite playerDeadSprite = null;
+
+    [SerializeField]
+    private List<Sprite> screamLevelSprites = new List<Sprite>();
+
+    private int screanLevelIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         EventManager.StartListening("PlayerDied", HandlePlayerDeadEvent);
+        EventManager.StartListening("ScreamLevelChanged", HandleScreamLevelChangedEvent);
+        EventManager.StartListening("Scream", HandleScreamEvent);
     }
 
     private void HandlePlayerDeadEvent(object data)
@@ -29,6 +39,31 @@ public class PlayerStateTracker : MonoBehaviour
         if (eventPlayerID == playerId)
         {
             spriteRenderer.sprite = playerDeadSprite;
+        }
+    }
+
+    private void HandleScreamLevelChangedEvent(object data)
+    {
+        int eventPlayerID = (int)data;
+
+        if (eventPlayerID == playerId)
+        {
+            if ((screanLevelIndex + 1) < screamLevelSprites.Count)
+            {
+                screanLevelIndex++;
+                screamLevelSpriteRenderer.sprite = screamLevelSprites[screanLevelIndex];
+            }
+        }
+    }
+
+    private void HandleScreamEvent(object data)
+    {
+        int eventPlayerID = (int)data;
+
+        if (eventPlayerID == playerId)
+        {
+            screanLevelIndex = 0;
+            screamLevelSpriteRenderer.sprite = screamLevelSprites[screanLevelIndex];
         }
     }
 }
