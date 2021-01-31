f04a80d31eee8a41317d4e57a2266c96cf38a2ea
Zeeshan Majeed <majeedzee@hotmail.com>
2021-01-31 14:19:50 -0500

    Adding player tracking functionality to UI game screen.

---
+++ Global Game Jam 2021/Assets/Anthony Scripts/PlayerStateTracker.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStateTracker : MonoBehaviour
+{
+    [SerializeField]
+    private int playerId = -1;
+
+    [SerializeField]
+    private Image spriteRenderer = null;
+
+    [SerializeField]
+    private Sprite playerAliveSprite = null;
+    [SerializeField]
+    private Sprite playerDeadSprite = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EventManager.StartListening("PlayerDied", HandlePlayerDeadEvent);
+    }
+
+    private void HandlePlayerDeadEvent(object data)
+    {
+        int eventPlayerID = (int)data;
+
+        if (eventPlayerID == playerId)
+        {
+            spriteRenderer.sprite = playerDeadSprite;
+        }
+    }
+}
